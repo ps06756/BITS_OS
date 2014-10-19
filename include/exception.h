5186c6691cb49476d969f026022e26d76db202c3
ps06756 <ps06756@gmail.com>
2014-10-19 12:50:12 +0530

    Deleted useless code(2)

--- include/exception.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXCEPTION_H
-#define EXCEPTION_H 
-#include <processor.h>
-extern "C" void isr0() ; 
-extern "C" void isr8() ;  
-/* This header file will define some common functions used to handle the exceptiosn. */ 
-extern "C" void divide_by_zero(breg val) ; // to be called from assembly code. (is0)
-extern "C" void double_fault(breg val) ;  // to be called from assembly code. (isr8) 
-#endif  
