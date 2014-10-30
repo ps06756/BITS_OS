1e27b4b58c39461d9a07fc292ef0811af9b89da9
ps06756 <ps06756@gmail.com>
2014-10-30 19:14:10 +0530

    Added commands for converting elf32 to raw binary with improved status handler

--- bootsect.asm
+++ bootsect.asm
@@ -1,26 +1,26 @@
-[org 0x7c00] ; we expect the code to be loaded at this position always and all the addresses expressed are relative to this. 
+;[org 0x7c00] ; we expect the code to be loaded at this position always and all the addresses expressed are relative to this. 
 ; This is the boot sector and will initially contain the only small relevant code. 
 ; We will have a full elf32 executable in the disk through which we will be booting our operating system and load the relevant part of the operating system in the ram. We will also support modules in the same way, 
-; O means that the operating system has started executing the code, and now it will check for the protected mode enable code.   
+; O means that the operating system has started executing the code, and now it will check for the protected mode enable code.  
 ; First setup the stack.
 ; ----------------------------------------------STACK_SETUP(1024 bytes)----------------------------------------------------------;  
+;extern p_char
+;extern disk_status
+;extern disk_reset
 mov ebp, 0x8000 
 mov esp, ebp 
 ;-----------------------------------------------TELETYPE_SETUP-------------------------------------------------------------------;
 mov al, 'L'
 call p_char 
 call disk_status
+call disk_reset
 jmp $ 
 ;----------------------------------------------METHODS_DEFINED_HERE-------------------------------------------------------------;
-fail:
+show_status:
 	add ah, '0'
 	mov al, ah 
 	call p_char
 	ret
-suc:
-	mov al, 'S' 
-	call p_char 
-	ret 
 p_char:
 	mov ah, 0x0e
 	int 10h 
@@ -28,10 +28,15 @@ p_char:
 disk_status: 
 	mov dl, 81h 
 	mov ah, 01  	
-	je suc
-	jne fail
+	int 13h 
+	call show_status
 	ret
-msg db 'Hello World',0 
+disk_reset:
+	mov dl, 81h
+	mov ah, 00
+	int 13h 
+	call show_status
+	ret
 ;-----------------------------------------------NULL_BYTES-----------------------------------------------------------------------;
 times 510 - ($ - $$) db 0
 ;-----------------------------------------------MAGIC_NUMBER---------------------------------------------------------------------; 
